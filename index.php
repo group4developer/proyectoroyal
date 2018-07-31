34738c54968d4fd845eaaa5f337ebfad317e3ab2
grupo4developer <orlanjack95@gmail.com>
2018-07-31 09:40:35 -0500

    v1

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+         <link rel="stylesheet" href="royal.css">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+         <div class="principal">
+
+        <header class="inicio" >
+                    <div class="banner_logo" >
+                            <p><img src="img/royal.jpg" alt="logo" id="logo"></p>
+            
+                    </div>
+                    <div class="sociales">
+                        
+                            <li id="soc"><a href="#" > <img src="img/face.png" alt="" width="30px" height="30px" value="Facebook" name="facebook"></a></li>
+                            <li id="soc2"><a href="#"> <img src="img/insta.png" alt="" width="30px" height="30px"></a></li>
+                                           
+                    </div>
+                    <div class="sesion">    
+    
+                            <li><a href="Login.html" id="login"> INICIAR SESION</a></li>
+                            <li><a href="registro.html" id="login"> REGISTRARSE</a></li>
+                           
+                    </div>
+                  
+                 
+               <div class="head1">     
+                <nav >
+                        <ul>
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="servicio.html">Servicios</a></li>
+                            <li><a href="promociones.html">Promociones</a></li>
+                          
+                            <li><a href="cotiza.html">Cotiza tu evento</a></li>
+                            <li><a href="contacto.html">Contacto</a></li>
+                        </ul>
+                    </nav>
+                </div> 
+               
+               
+                
+                
+                
+                    
+          
+        </header>
+     
+        <div class="col_left">
+            <section class="fondo_index">  
+                <div class="fondo_color">
+
+                </div> 
+            </section>
+      
+                   
+        
+           
+            <section class="servicios">
+               
+                <div class="img_ser">
+                    <p><img src="img/portada.jpg" alt="img_servicio" id="img_servicio">  </p>
+                </div>
+                <div class="art_ser">
+                    <h2 id="title1">SOMOS ROYAL</h2>
+                    <p class="a_servicios">
+                        
+                        Respaldados por una trayectoria de 10 años, Salon de eventos "Royal" ofrece Asesoría Integral para la Organización de Eventos Sociales y Corporativos, cuidando con profesionalismo y calidad cada uno de los detalles que harán de su evento un momento inolvidable. Profesionales especializados en Europa y Medio Oriente se encargarán de deleitarle con lo mejor de la comida gourmet nacional e internacional. Descubra nuevas tendencias en lo que se refiere al Mágico Mundo de las Bodas y permítanos soñar junto a usted el día más importante de su vida!. Trabajamos con una amplia variedad de quintas y salones de recepción, para que usted pueda elegir el que mejor se adapte a sus gustos y preferencias. Servicio a Nivel Nacional!
+
+                    </p>
+                </div>
+                <section>
+                    <div class="title_ser">
+                        <h2 id="title_ser">nuestros servicios</h2>
+                    </div>
+
+
+                </section>
+            </section>
+            <section class="promociones">
+                
+                   
+                        <div class="art_ser">
+                            <h2 id="title1">SOMOS ROYAL</h2>
+                            <p class="a_servicios">
+                                
+                                    Somos especialistas en realizar eventos con los mas altas exigencias...
+        
+                            </p>
+                        </div>
+                        <div class="img_ser">
+                                <p><img src="img/portada.jpg" alt="img_servicio" id="img_servicio">  </p>
+                            </div>
+                 
+                    <div class="imagenes">
+
+                        <div class="img_prom1">
+                            <img src="img/eventos_corp.jpg" alt="img_servicio" id="img_promo"  > 
+                            <article class="p1"></article>
+                        </div>
+                        <div class="img_prom2">
+                            <img src="img/eventos_sociales.jpg" alt="img_servicio" id="img_promo"  >  
+                            <article class="p2">Reuniones, Lanzamiento de productos, Festejos, etc. Lo ayudamos en su mejor negocio</article>
+                        </div>
+                        <div class="img_prom3">
+                             <img src="img/comida.jpg" alt="img_servicio" id="img_promo"  >
+                             <article class="p3">Reuniones, Lanzamiento de productos, Festejos, etc. Lo ayudamos en su mejor negocio</article>  
+                          
+                        </div>
+                        <div class="img_prom4">
+                                <img src="img/comida.jpg" alt="img_servicio" id="img_promo"  >
+                                <article class="p4">Reuniones, Lanzamiento de productos, Festejos, etc. Lo ayudamos en su mejor negocio</article>  
+                             
+                        </div>
+                        <div class="img_prom5">
+                                <img src="img/comida.jpg" alt="img_servicio" id="img_promo"  >
+                                <article class="p5">Reuniones, Lanzamiento de productos, Festejos, etc. Lo ayudamos en su mejor negocio</article>  
+                             
+                           </div>
+                           <div class="img_prom6">
+                                <img src="img/comida.jpg" alt="img_servicio" id="img_promo"  >
+                                <article class="p6">Reuniones, Lanzamiento de productos, Festejos, etc. Lo ayudamos en su mejor negocio</article>  
+                             
+                           </div>
+                    </div>
+            </section>
+
+        </div>
+        <footer class="final">
+            
+            <div class="img_footer">
+                <p><img src="img/royal.png" alt="imagen_footer" id="img_foot"></p>
+            </div>
+            
+            <div class="derechos">
+                    <h5>Todos los derechos reservados © "Royal" Corp  Quito-Ecuador  by </h5>
+        
+                    </div>
+        </footer>
+        
+       
+    </div>
+        <?php
+        
+        ?>
+    </body>
+</html>
